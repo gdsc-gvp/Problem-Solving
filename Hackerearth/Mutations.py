5e8c702ef55b2e8b957fb5c5fd60e858105efd2c
SURYACODER <60681084+SURYACODER@users.noreply.github.com>
2021-10-01 22:40:31 +0530

    Create Mutations.py

---
+++ Hackerearth/Mutations.py
@@ -0,0 +1,11 @@
+def mutate_string(string, position, character):
+    l = list(string)
+    l[position] = character
+    l = "".join(l)
+    return l
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
