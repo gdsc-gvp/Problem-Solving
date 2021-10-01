ee9c8c72a33310de826232dc6741945e88ceeff2
prampratap30 <prampratap30@gmail.com>
2021-10-01 19:15:48 +0530

    chore: add a problem from a codeforces contest

---
+++ Codeforces/1560A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// problem statement: https://codeforces.com/problemset/problem/1560/A
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int k;
+		cin >> k;
+
+		for (int i = 1; ; i++)
+		{
+			if (i % 3 == 0 || i % 10 == 3)
+				continue;
+			if (--k == 0)
+			{
+				cout << i << '\n';
+				break;
+			}
+		}
+	}
+}
